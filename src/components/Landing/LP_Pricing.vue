e80acb9fa87b14b6df97ab92f9a2acb18b17ec94
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-06-23 14:45:27 +0200

    Ajout du lien de redirection contact pour envoie de mail

--- src/components/Landing/LP_Pricing.vue
+++ src/components/Landing/LP_Pricing.vue
@@ -87,7 +87,7 @@
                 <span>Support prioritaire et mises à jour automatiques des taux</span>
                 </li>
             </ul>
-            <a href="contactto:contact@jeremiecode.fr">
+            <a href="mailto:contact@jeremiecode.fr">
                 <button class="text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 w-full">Essayer Pro maintenant</button>
             </a>
             </div>
